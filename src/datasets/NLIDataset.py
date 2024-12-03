0b3adc7edbb6798ae089ed6c1b98b285a75c7d43
Matteo Boglioni <boglioni.matteo@gmail.com>
2024-12-03 18:46:52 +0100

    update dataset classes

--- src/datasets/NLIDataset.py
+++ src/datasets/NLIDataset.py
@@ -10,16 +10,16 @@ class NLIDataset(Dataset):
         self.preprocess()
 
     def tokenize_function(self, samples):
-        raise NotImplementedError("Subclasses should implement this method.")
+        raise NotImplementedError('Subclasses should implement this method.')
     
     def change_labels(self, samples):
-        raise NotImplementedError("Subclasses should implement this method.")
+        raise NotImplementedError('Subclasses should implement this method.')
 
     def preprocess(self):
         self.dataset = self.dataset.map(self.tokenize_function, batched=True) 
         self.dataset = self.dataset.map(self.change_labels, batched=True)
-        self.dataset = self.dataset.rename_column("label", "labels") 
-        #self.dataset = self.dataset.select_columns(["input_ids", "attention_mask", "labels"])
+        self.dataset = self.dataset.rename_column('label', 'labels') 
+        self.dataset = self.dataset.select_columns(['input_ids', 'attention_mask', 'labels'])
 
     def __len__(self):
         return len(self.dataset)
@@ -36,13 +36,14 @@ class NLIDataset(Dataset):
         }
     
 class SNLIDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="stanfordnlp/snli", split='train'):
-        dataset = load_dataset(dataset_name, cache_dir="datasets", split=split)
+
+    def __init__(self, tokenizer, dataset_name='stanfordnlp/snli', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, cache_dir=cache_dir, split=split)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing SNLI-style Hugging Face dataset")
-        self.dataset = self.dataset.filter(lambda example: example["label"] != -1 and example["label"] != 1)
+        print(f'Processing SNLI-style Hugging Face dataset')
+        self.dataset = self.dataset.filter(lambda example: example['label'] != -1 and example['label'] != 1)
         super().preprocess()
         
     def change_labels(self,batch):
@@ -53,17 +54,18 @@ class SNLIDataset(NLIDataset):
      
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["premise"], batch["hypothesis"], truncation=True, padding="max_length", max_length=128
-        )
-    
+        batch['premise'], batch['hypothesis'], truncation=True, padding='max_length', max_length=128
+        )        
+
 class SciTailDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="allenai/scitail", format= "dgem_format", split='train'):
-        dataset = load_dataset(dataset_name, format,cache_dir="datasets", split=split)
+
+    def __init__(self, tokenizer, dataset_name='allenai/scitail', format= 'dgem_format', split='train', cache_dir = 'datasets'):
+        dataset = load_dataset(dataset_name, format,cache_dir=cache_dir, split=split)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing SciTail-style Hugging Face dataset")
-        self.dataset = self.dataset.filter(lambda example: example["label"] != 'neutral')
+        print(f'Processing SciTail-style Hugging Face dataset')
+        self.dataset = self.dataset.filter(lambda example: example['label'] != 'neutral')
         super().preprocess()
         
     def change_labels(self,batch):
@@ -75,40 +77,58 @@ class SciTailDataset(NLIDataset):
 
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["premise"], batch["hypothesis"], truncation=True, padding="max_length", max_length=128
+        batch['premise'], batch['hypothesis'], truncation=True, padding='max_length', max_length=128
         )
 
 class GLUEDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="nyu-mll/glue", subset= "wnli", split='train'):
-        dataset = load_dataset(dataset_name, subset ,cache_dir="datasets", split=split)
-        self.subset = subset
+
+    def __init__(self, tokenizer, dataset_name='nyu-mll/glue', subset= 'wnli', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, subset ,cache_dir=cache_dir, split=split)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing GLUE-style Hugging Face dataset")
+        print(f'Processing GLUE-style Hugging Face dataset')
         super().preprocess()
 
     def change_labels(self,batch):
-        if self.subset == 'wnli':
-            values = np.zeros(len(batch['label']), dtype=int)
-            values[np.where(np.array(batch['label']) == 0)[0]] = 1 
-            values[np.where(np.array(batch['label']) == 1)[0]] = 0
-            batch['label'] = values
-            print(batch['label'])
         return batch  
 
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["sentence1"], batch["sentence2"], truncation=True, padding="max_length", max_length=128
+        batch['sentence1'], batch['sentence2'], truncation=True, padding='max_length', max_length=128
         )
+    
+class RTEDataset(GLUEDataset):
+
+    def __init__(self, tokenizer, dataset_name='nyu-mll/glue', subset= 'rte', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, subset ,cache_dir=cache_dir, split=split)
+        super(GLUEDataset, self).__init__(tokenizer, dataset)
+
+class WNLIDataset(GLUEDataset):
+
+    def __init__(self, tokenizer, dataset_name='nyu-mll/glue', subset= 'wnli', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, subset ,cache_dir=cache_dir, split=split)
+        super(GLUEDataset, self).__init__(tokenizer, dataset)
+    
+    def change_labels(self, batch):
+        values = np.zeros(len(batch['label']), dtype=int)
+        values[np.where(np.array(batch['label']) == 0)[0]] = 1 
+        values[np.where(np.array(batch['label']) == 1)[0]] = 0
+        batch['label'] = values
+        return batch
+
+class MNLIDataset(SNLIDataset):
+    def __init__(self, tokenizer, dataset_name='nyu-mll/glue', subset= 'mnli',split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, subset,cache_dir=cache_dir, split=split)
+        super(SNLIDataset, self).__init__(tokenizer, dataset)
 
 class PAWSDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="google-research-datasets/paws", subset= "labeled_final", split='train'):
-        dataset = load_dataset(dataset_name, subset ,cache_dir="datasets", split=split)
+    def __init__(self, tokenizer, dataset_name='google-research-datasets/paws', subset= 'labeled_final', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, subset ,cache_dir=cache_dir, split=split)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing PAWS-style Hugging Face dataset")
+        print(f'Processing PAWS-style Hugging Face dataset')
         super().preprocess()
 
     def change_labels(self,batch):
@@ -121,16 +141,16 @@ class PAWSDataset(NLIDataset):
         
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["sentence1"], batch["sentence2"], truncation=True, padding="max_length", max_length=128
+        batch['sentence1'], batch['sentence2'], truncation=True, padding='max_length', max_length=128
         )
 
 class HANSDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="jhu-cogsci/hans", split='train'):
-        dataset = load_dataset(dataset_name, split=split, trust_remote_code=True)
+    def __init__(self, tokenizer, dataset_name='jhu-cogsci/hans', split='train', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, split=split, trust_remote_code=True, cache_dir=cache_dir)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing Hans-style Hugging Face dataset")
+        print(f'Processing Hans-style Hugging Face dataset')
         super().preprocess()
         
     def change_labels(self,batch):
@@ -138,17 +158,17 @@ class HANSDataset(NLIDataset):
      
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["premise"], batch["hypothesis"], truncation=True, padding="max_length", max_length=128
+        batch['premise'], batch['hypothesis'], truncation=True, padding='max_length', max_length=128
         )
 
 class ANLIDataset(NLIDataset):
-    def __init__(self, tokenizer, dataset_name="facebook/anli", split='train_r3'):
-        dataset = load_dataset(dataset_name, split=split)
+    def __init__(self, tokenizer, dataset_name='facebook/anli', split='train_r3', cache_dir='datasets'):
+        dataset = load_dataset(dataset_name, split=split, cache_dir=cache_dir)
         super().__init__(tokenizer, dataset)
 
     def preprocess(self):
-        print(f"Processing anli-style Hugging Face dataset")
-        self.dataset = self.dataset.filter(lambda example: example["label"] != 1)
+        print(f'Processing anli-style Hugging Face dataset')
+        self.dataset = self.dataset.filter(lambda example: example['label'] != 1)
         super().preprocess()
         
     def change_labels(self,batch):
@@ -159,9 +179,7 @@ class ANLIDataset(NLIDataset):
      
     def tokenize_function(self,batch):
         return self.tokenizer(
-        batch["premise"], batch["hypothesis"], truncation=True, padding="max_length", max_length=128
+        batch['premise'], batch['hypothesis'], truncation=True, padding='max_length', max_length=128
         )
 
 
-
-    
