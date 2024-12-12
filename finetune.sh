5d9a1febdc8d42dc463f4dba160fa9f803b1a59e
Matteo Boglioni <boglioni.matteo@gmail.com>
2024-12-12 16:33:00 +0100

    update deepspeed script and configs

---
+++ finetune.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=mia
+#SBATCH --gpus=rtx_4090:4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --ntasks=1 --cpus-per-task=16
+#SBATCH --time=100:00:00
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+deepspeed --num_gpus=4 src/train.py --config results/config_opt.toml
